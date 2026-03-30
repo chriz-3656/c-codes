9f09168901ed699266f86f0d3fe2a6d1400d80b6
Chris <chriz3656@users.noreply.github.com>
2026-03-30 20:50:20 +0530

    Simplify average of three numbers

--- code/average_of_three_numbers.c
+++ code/average_of_three_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-double avg(int a, int b, int c);
+float avg(int a, int b, int c);
 
-int main(void)
+int main()
 {
     int a, b, c;
-    double d;
+    float d;
 
     printf("Enter three numbers: ");
     scanf("%d %d %d", &a, &b, &c);
@@ -14,7 +14,7 @@ int main(void)
     return 0;
 }
 
-double avg(int a, int b, int c)
+float avg(int a, int b, int c)
 {
-    return (a + b + c) / 3.0;
+    return (a + b + c) / 3.0f;
 }
